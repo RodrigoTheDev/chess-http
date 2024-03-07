bd205017221e45e01feabfe90de1a2c07378fae0
JGSilva <jgoliveira445@gmail.com>
2024-03-07 09:27:09 -0300

    feat: passando a difucldade pro sessão

--- xadrez.py
+++ xadrez.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta'
 
 @app.route('/')
-def index():
+def index():  
     return render_template('index.html')
 
-@app.route('/config')
+@app.route('/config', methods=['GET', 'POST'])
 def config():
-    return render_template('config.html')
+    if request.method == 'POST':
+        session['dificuldade'] = int(request.form['dificuldade'])
+        return redirect(url_for('index'))
+    else:
+        return render_template('config.html')
 
 @app.route('/jogar')
 def jogar():
-    return render_template('jogar.html')
+    dificuldade = session.get('dificuldade', None)
+    
+    if dificuldade == None:
+        session['dificuldade'] == 0
+  
+    return render_template('jogar.html', dificuldade=dificuldade)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
